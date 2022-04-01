6b76af168781634308597b890930a61680ed3454
Pimenta <mateuspimartins@hotmail.com>
2022-03-31 22:52:01 -0300

    meus habitos new habit

--- src/components/Habitos.js
+++ src/components/Habitos.js
@@ -5,21 +5,46 @@ import Footer from './Headers and Footers/Footer';
 export default function Habitos() {
   return (
     <>
-    <Header/>
-    <Container>
-      
+      <Header />
+      <Container>
 
-      <div className="habitos">
-        <div className="habitos-item">
-          <div className="habitos-title">
-            <h1>Meus hábitos</h1>
-            <button className="addHabit">+</button>
-            
+
+        <div className="habitos">
+          <div className="habitos-item">
+            <div className="habitos-title">
+              <h1>Meus hábitos</h1>
+              <button className="addHabit">+</button>
+            </div>
+
+            <div className="center-habits">
+              <div className="create-habito">
+                <input type="text" placeholder="Nome do hábito" />
+                <div className="habito-days">
+                  <button>D</button>
+                  <button>D</button>
+                  <button>D</button>
+                  <button>D</button>
+                  <button>D</button>
+                  <button>D</button>
+                  <button>D</button>
+                </div>
+                <div className="cancel-save">
+                  <button className="save">Salvar</button>
+                  <button className="cancel">Cancelar</button>
+
+                </div>
+              </div>
+            </div>
+
+
+            <div className="semHabito">
+              <h2>Você não tem nenhum hábito cadastrado ainda.
+                Adicione um hábito para começar a trackear!</h2>
+            </div>
           </div>
         </div>
-      </div>
-    </Container>
-    <Footer/>
+      </Container>
+      <Footer />
     </>
   );
 }
@@ -32,6 +57,95 @@ flex-direction: column;
 background: #E5E5E5;
 height: calc(100vh - 84px);
 width: 100vw;
+
+.cancel-save{
+  display: flex;
+  flex-direction: row-reverse;
+  margin-top: 32px;
+
+  button{
+    width: 84px;
+height: 35px;
+
+background: #52B6FF;
+border-radius: 4.63636px;
+font-family: 'Lexend Deca';
+font-style: normal;
+font-weight: 400;
+font-size: 15.976px;
+line-height: 20px;
+
+text-align: center;
+
+color: #FFFFFF;
+  }
+}
+
+.center-habits{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+.create-habito{
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+  padding-right: 18px;
+  width: 340px;
+  height: 180px;
+  background: #FFFFFF;
+border-radius: 5px;
+  margin-top: 22px;
+  margin-right: 40px;
+
+  input{
+height: 45px;
+left: 36px;
+top: 165px;
+padding-left: 11px;
+
+background: #FFFFFF;
+border: 1px solid #D5D5D5;
+box-sizing: border-box;
+border-radius: 5px;
+
+font-family: 'Lexend Deca';
+font-style: normal;
+font-weight: 400;
+font-size: 19.976px;
+line-height: 25px;
+
+color: #DBDBDB;
+  }
+
+  button{
+    background: #FFFFFF;
+border: 1px solid #D5D5D5;
+box-sizing: border-box;
+border-radius: 5px;
+color: grey;
+margin: 2px;
+margin-top: 8px;
+}
+}
+
+.semHabito{
+  margin: 13px;
+  margin-top: 30px;
+  margin-right: 30px;
+
+  h2{
+    font-family: 'Lexend Deca';
+font-style: normal;
+font-weight: 400;
+font-size: 17.976px;
+line-height: 22px;
+
+color: #666666;
+  }
+}
   
 .habitos{
   display: flex;
